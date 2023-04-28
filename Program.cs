e875d775c81f2c4abed3808fd11e1065cee87421
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-04-28 10:45:22 +0700

    /

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using PZ_4_Lamaev;
+using System;
+ using System.Threading;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+       
+        
+        static void Main(string[] args)
+        {
+            Thread currentThread = Thread.CurrentThread;
+            Time time1 = new Time();
+            Console.WriteLine("Введите часы");
+            time1.hour = Convert.ToInt32(Console.ReadLine());
+            currentThread.Name = time1.hour.ToString();
+            Console.WriteLine($"Имя потока: {time1}");
+            Console.WriteLine($"Запущен ли поток: {currentThread.IsAlive}");
+            Console.WriteLine($"Id потока: {currentThread.ManagedThreadId}");
+            Console.WriteLine($"Приоритет потока: {currentThread.Priority}");
+            Console.WriteLine($"Статус потока: {currentThread.ThreadState}");
+            Console.WriteLine("Введите минуты");
+            time1.minute = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите секунды");
+            time1.second = Convert.ToInt32(Console.ReadLine());
+            time1.Display();
+
+
+
+
+
+        }
+    }
+}
---
+++ Time.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_4_Lamaev
+{
+    public class Time 
+    {
+        public int hour;
+        public int minute;
+        public int second;
+        
+
+        //public Time(int hours, int minutes, int seconds)
+        //{
+        //    hour = hours;
+        //    minute = minutes;
+        //    second = seconds;
+        //}
+
+        public void Read()
+        {
+            Convert.ToInt32(Console.ReadLine());
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"часы:{hour} минуты: {minute} секунды: {second}");
+        }
+    }
+
+        
+    }
+    
+
